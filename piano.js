70b2563944ade70aa2de252ea9a15713d1044953
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2022-02-21 15:53:05 +0400

    callbacks,sust-click,css

--- piano.js
+++ piano.js
@@ -9,32 +9,47 @@ chars.forEach((char) => {
   keys[`${char}`] = document.querySelector(`.key-${char}`);
 });
 
+/////////////////////////////////////////////
 // SUSTAIN PEDAL
+
 let isSustained = false;
-document.body.addEventListener("keydown", (e) => {
+const sustainON = function () {
+  isSustained = true;
+  sustain.style.backgroundColor = "#262";
+  pedalStatus.textContent = "ON";
+};
+const sustainOFF = function () {
+  isSustained = false;
+
+  sustain.style.backgroundColor = "#444";
+  pedalStatus.textContent = "OFF";
+
+  const playingNotes = chars
+    .filter(
+      (char) =>
+        !notes[`${char}`].paused &&
+        !keys[`${char}`].className.includes("pressed")
+    )
+    .map((playingChar) => notes[`${playingChar}`]);
+
+  playingNotes.forEach((playingNote) => playingNote.pause());
+};
+
+// Click Toggle
+
+sustain.addEventListener("click", () =>
+  isSustained ? sustainOFF() : sustainON()
+);
+
+document.addEventListener("keydown", (e) => {
   if (e.repeat) return;
   if (`${e.key}`.toLowerCase() === "p") {
-    isSustained = true;
-    sustain.style.backgroundColor = "#262";
-    pedalStatus.textContent = "ON";
+    sustainON();
   }
 });
-document.body.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", (e) => {
   if (`${e.key}`.toLowerCase() === "p") {
-    isSustained = false;
-
-    sustain.style.backgroundColor = "#444";
-    pedalStatus.textContent = "OFF";
-
-    const playingNotes = chars
-      .filter(
-        (char) =>
-          !notes[`${char}`].paused &&
-          !keys[`${char}`].className.includes("pressed")
-      )
-      .map((playingChar) => notes[`${playingChar}`]);
-
-    playingNotes.forEach((playingNote) => playingNote.pause());
+    sustainOFF();
   }
 });
 
@@ -44,6 +59,7 @@ const play = (key) => {
   keys[`${key}`].classList.add("pressed");
   notes[`${key}`].currentTime = 0;
   notes[`${key}`].play();
+  console.log("play");
 };
 
 const stop = (key) => {
@@ -52,31 +68,50 @@ const stop = (key) => {
   keys[`${key}`].classList.remove("pressed");
 
   !isSustained && notes[`${key}`].pause();
+  console.log("stop");
 };
 
+/////////////////////////////////////////////
 // KEYBOARD
-document.body.addEventListener("keydown", (e) => {
+
+const handleKeyDown = function (e) {
   if (e.repeat) return;
-  // console.log(e.key);
   play(e.key);
-});
-document.body.addEventListener("keyup", (e) => {
+};
+const handleKeyUp = function (e) {
   stop(e.key);
-});
+};
+document.body.addEventListener("keydown", handleKeyDown);
+document.body.addEventListener("keyup", handleKeyUp);
 
+/////////////////////////////////////////////
 // TOUCH
-const keyFromDOM = (e) => e?.srcElement?.classList[2]?.slice(-1);
-document.body.addEventListener("touchstart", (e) => {
-  play(keyFromDOM(e));
-});
-document.body.addEventListener("touchend", (e) => {
-  stop(keyFromDOM(e));
-});
 
-// CLICK
-document.body.addEventListener("mousedown", (e) => {
+const keyFromDOM = (e) => e?.target?.closest(".key")?.classList[2]?.slice(-1);
+
+const handleTouchStart = function (e) {
+  e.stopPropagation();
   play(keyFromDOM(e));
-});
-document.body.addEventListener("mouseup", (e) => {
+};
+const handleTouchEnd = function (e) {
+  e.stopPropagation();
   stop(keyFromDOM(e));
-});
+};
+
+document.body.addEventListener("touchstart", handleTouchStart);
+document.body.addEventListener("touchend", handleTouchEnd);
+
+///////////////////////////////////////////
+// CLICK
+
+const handleMouseDown = function (e) {
+  e.stopPropagation();
+  play(keyFromDOM(e));
+};
+const handleMouseUp = function (e) {
+  e.stopPropagation();
+  stop(keyFromDOM(e));
+};
+
+document.body.addEventListener("mousedown", handleMouseDown);
+document.body.addEventListener("mouseup", handleMouseUp);
